4950a7e11029ce7731183c25395808bd411019cb
Ardian Shala <ardian.sh@gmx.de>
2019-11-13 14:37:39 +0100

    init

---
+++ App.vue
@@ -0,0 +1,26 @@
+<script>
+    import ButtonCounter from "./components/ButtonCounter.vue";
+
+    export default {
+        components: { ButtonCounter },
+        data() {
+            return {
+                title: "Vuejs & Parcel & JSX"
+            };
+        },
+        render(h) {
+            return (
+                <div>
+                    <h1>{this.title}</h1>
+                    <ButtonCounter />
+                </div>
+            );
+        }
+    };
+</script>
+
+<style lang="scss" scoped>
+    .container {
+        color: blue;
+    }
+</style>
---
+++ components/ButtonCounter.vue
@@ -0,0 +1,21 @@
+<script>
+    export default {
+        data() {
+            return {
+                count: 0
+            };
+        },
+        methods: {
+            increment() {
+                this.count++;
+            }
+        },
+        render(h) {
+            return (
+                <button on-click={() => this.increment()}>
+                    You clicked me {this.count} times.
+                </button>
+            );
+        }
+    };
+</script>
